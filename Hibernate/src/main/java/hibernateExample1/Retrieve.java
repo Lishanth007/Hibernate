f90f867950a8134aa56965551b1234915e0f03be
Lishanth <120487924+Lishanth007@users.noreply.github.com>
2023-11-06 11:00:09 +0530

    retrieve is commited

--- Hibernate/src/main/java/hibernateExample1/Retrieve.java
+++ Hibernate/src/main/java/hibernateExample1/Retrieve.java
@@ -1,9 +1,20 @@
 package hibernateExample1;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 public class Retrieve {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SessionFactory sf = HibernateConfiguration.provideSf();
+		Session ses = sf.openSession();
+		Transaction ts = ses.beginTransaction();
+
+		Myclass s1 = ses.get(Myclass.class, 1);
+		System.out.println("Student name: " + s1.getSname());
+
+		ts.commit();
 
 	}
 
